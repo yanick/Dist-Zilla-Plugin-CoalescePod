8d43c1c5e34910ec53f49b29690cd38d4bdaec00
Yanick Champoux <yanick@babyl.dyndns.org>
2012-02-04 13:07:49 -0500

    Build results of e645c1f (on master)

--- lib/Dist/Zilla/Plugin/CoalescePod.pm
+++ lib/Dist/Zilla/Plugin/CoalescePod.pm
@@ -1,6 +1,12 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::CoalescePod;
+BEGIN {
+  $Dist::Zilla::Plugin::CoalescePod::AUTHORITY = 'cpan:YANICK';
+}
+{
+  $Dist::Zilla::Plugin::CoalescePod::VERSION = '0.1.0';
+}
 # ABSTRACT: merge .pod files into their .pm counterparts
 
 use Moose;
@@ -25,3 +31,38 @@ sub munge_file {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::CoalescePod - merge .pod files into their .pm counterparts
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+    # in dist.ini
+    [CoalescePod]
+
+=head1 DESCRIPTION
+
+If the files I<Foo.pm> and I<Foo.pod> both exist, the content of the pod file is
+appended at the end of the C<.pm>, and the pod file is removed.
+
+=head1 AUTHOR
+
+Yanick Champoux <yanick@babyl.dyndns.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Yanick Champoux.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
