386d2e479c790e35260458b042c4ac44cd7517ce
Yanick Champoux <yanick@babyl.dyndns.org>
2013-07-13 14:54:11 -0400

    only coalesce under /lib

--- lib/Dist/Zilla/Plugin/CoalescePod.pm
+++ lib/Dist/Zilla/Plugin/CoalescePod.pm
@@ -11,12 +11,15 @@ with 'Dist::Zilla::Role::FileMunger';
 sub munge_file {
     my ( $self, $file ) = @_;
 
-    return unless $file->name =~ /\.pm$/;
+    # only look under /lib
+    return unless $file->name =~ m#^lib/.*\.pm$#;
 
     ( my $podname = $file->name ) =~ s/\.pm$/.pod/;
 
     my ( $podfile ) = grep { $_->name eq $podname } 
                            @{ $self->zilla->files } or return;
+
+   $self->log( "merged $podfile into " . $file->name );
 
     my @content = split /(^__DATA__$)/m, $file->content;
 
